580f6746fa04c8b20d74b30f75f86a021d37997a
wangkun <wangkun@cmiot.chinamobile.com>
2019-12-09 18:32:37 +0800

    尝试实现CMD异步执行接口

--- Program.cs
+++ Program.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.IO.Ports;
+using System.Threading;
+using System.Collections.Concurrent;
 
 namespace WindowsFormsApp1
 {
@@ -18,31 +20,32 @@ namespace WindowsFormsApp1
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Form1 mainForm = new Form1();
-            DataSource dataSource = new DataSource(mainForm);
-            dataSource.Open("COM10");
+            FlowSensor sensor = new FlowSensor();
+            sensor.Open("COM10");
             Application.Run(mainForm);
         }
     }
 
-    class DataSource
+    /* 流速传感器 */
+    class FlowSensor
     {
+        public enum FrameType { CmdResp, DataPoint };
+        public delegate void FrameRecvHandler(FrameType type, object frameData);
+        public event FrameRecvHandler FrameRecved;
+
         private SerialPort m_serialPort = null;
-        private Form1 m_mainForm = null;
         private WaveDecoder m_waveDecoder = new WaveDecoder();
+        private SerialDataReceivedEventHandler m_serialDataRecvHandler;
+        private string m_currentCmdResp = null;
 
-        public DataSource(Form1 mainForm)
+        public FlowSensor()
         {
-            m_mainForm = mainForm;
-
             m_waveDecoder.Test();
         }
 
         public void Open(string portName)
         {
-            if (null != m_serialPort)
-            {
-                m_serialPort.Close();
-            }
+            m_serialPort.Close();
             m_serialPort = new SerialPort(portName);
             m_serialPort.BaudRate = 115200;
             m_serialPort.Parity = Parity.None;
@@ -51,18 +54,49 @@ namespace WindowsFormsApp1
             m_serialPort.Handshake = Handshake.None;
             m_serialPort.RtsEnable = true;
 
-            m_serialPort.DataReceived += new SerialDataReceivedEventHandler(DataReceivedHandler);
+            m_serialDataRecvHandler = new SerialDataReceivedEventHandler(DataReceivedHandler);
+            m_serialPort.DataReceived += m_serialDataRecvHandler;
 
             m_serialPort.Open();
         }
 
         public void Close()
         {
-            if (null != m_serialPort)
-            {
-                m_serialPort.Close();
-                m_serialPort = null;
-            }
+            m_serialPort.Close();
+            m_serialPort = null;
+        }
+
+        public async Task<string> ExcuteCmdAysnc(string cmd, int timeout)
+        {
+            m_serialPort.Write(cmd);
+
+            string cmdResp = await Task.Run(() => {
+                string result = "";
+                int timeoutCnt = (timeout > 0) ? timeout : 0;
+                lock (m_currentCmdResp)
+                {
+                    if (timeoutCnt > 0)
+                    {
+                        while ((null == m_currentCmdResp) && (timeoutCnt > 0))
+                        {
+                            Thread.Sleep(1);
+                            --timeoutCnt;
+                        }
+                    }
+                    else
+                    {
+                        while (null == m_currentCmdResp)
+                        {
+                            Thread.Sleep(1);
+                        }
+                    }
+                    result = m_currentCmdResp;
+                    m_currentCmdResp = null;
+                }
+                return result;
+            });
+
+            return cmdResp;
         }
 
         private void DataReceivedHandler(
@@ -74,7 +108,15 @@ namespace WindowsFormsApp1
             byte[] dataBuf = new byte[dataLen];
             sp.Read(dataBuf, 0, dataLen);
             List<WaveDecoder.WaveData> dataList = m_waveDecoder.FrameDecode(dataBuf);
-            m_mainForm.RecvData(dataList);
+            // TODO
+            lock(m_currentCmdResp)
+            {
+                if (null == m_currentCmdResp)
+                {
+                    m_currentCmdResp = "";
+                }
+            }
+            // TODO
         }
     }
 }
